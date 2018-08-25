866edf75a53dfb2a286e7a91f9b3342e5ef315fa
ws <921891748@qq.com>
2018-08-25 15:38:43 +0800

    'x'

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  //BASE_API: '"http://crm.ai-xg.com/api/web/api/v1/"'
-  BASE_API: '"http://localhost/web/api/v1/"'
+  BASE_API: '"http://crm.ai-xg.com/api/web/api/v1/"'
+  //BASE_API: '"http://localhost/web/api/v1/"'
 }
